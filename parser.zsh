5b01d4be337312d5d7bc32594a6ec85ff9af5416
jimmijj <jimmij@o2.pl>
2016-04-06 16:48:28 +0100

    Corrected highlighting of double quoted substrings.

--- parser.zsh
+++ parser.zsh
@@ -133,7 +133,7 @@ _check_common_expression()
 	'"'*'"')
 	    style="${__chromatic_attrib_zle[comments]}"
 	    region_highlight+=("$start_pos $end_pos $style")
-	    _substring
+	    _substring "$arg" "$((init_pos+start_pos))"
 	    issubstring=1
 	    ;;
 	'$'[-#'$''*'@?!]|'$'[a-zA-Z0-9_]##) style="${__chromatic_attrib_zle[parameters]}";;
